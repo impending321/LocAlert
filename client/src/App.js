b66ed6773d65b313705edbf5f1036b217d420136
Ansh <varshneyansh321@gmail.com>
2024-03-18 21:08:04 +0530

    Work initiated

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="client\public\LocAlert(LOGO).jpg" className="App-logo" alt="logo" />
+        <p>
+          Work under Progress!!
+        </p>
+        {/* <a
+          className="App-link"
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Creating...
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
