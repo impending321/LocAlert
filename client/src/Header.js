533da78bc78642f7b3496ebd0479ad26f6543b4d
Ansh <varshneyansh321@gmail.com>
2024-03-19 22:03:53 +0530

    icon and responsive header.

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,4 +1,6 @@
 import image from './assets/LocAlert(LOGO).jpg';
+import font from './assets/loc(Font).png';
+
 import { MapIcon, UserIcon } from '@heroicons/react/outline';
 
 function Header() {
@@ -14,23 +16,25 @@ function Header() {
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     const formattedDate = currentDate.toLocaleDateString(undefined, options);
     return (
-      <header className="flex justify-between items-center w-full h-[10vh] bg-logoyellow p-[2vh]">
+      <header className="flex justify-between items-center w-full h-16 sm:h-20 bg-logoyellow p-4 sm:p-6">
         <div className="flex items-center">
-          <img src={image} alt="logo" className="w-[10vh] h-w m-[2vh] ml-[0vh]" />
-          <p className="text-logodark text-lg">LocAlert</p>
+          <img src={image} alt="logo" className="w-12 h-12 sm:w-16 sm:h-16 m-2 sm:m-4" />
+          <img src={font} alt="font" className="w-36 sm:w-48 h-auto m-2 sm:m-4" />
+          {/* <p className="text-logodark text-lg">LocAlert</p> */}
         </div>
-        <div className="flex items-center">
-        <p className='text-logodark text-lg ml-[60vh] mr-[2vh]'>{formattedDate}</p>
+        <div className="flex items-center ml-auto">
+          <p className='text-logodark text-lg mr-4 sm:mr-8'>{formattedDate}</p>
           <form className="flex">
-            <button onClick={handleMapIcon} className='h-[10vh] w-[5vh] mr-[2vh]'>
-              <MapIcon className=" text-logodark" />
+            <button onClick={handleMapIcon} className='h-8 w-8 sm:h-8 sm:w-8 mr-2 sm:mr-4'>
+              <MapIcon className="text-logodark" />
             </button>
-            <button onClick={handleUserIcon} className="h-[10vh] w-[5vh] mr-[2vh]">
-              <UserIcon  className="text-logodark" />
+            <button onClick={handleUserIcon} className="h-8 w-8 sm:h-8 sm:w-8">
+              <UserIcon className="text-logodark" />
             </button>
           </form>
         </div>
       </header>
     );
-  }
-  export {Header};
+}
+
+export { Header };
